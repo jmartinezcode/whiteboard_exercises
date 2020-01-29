09a5fe012ed9d5d8e4b9a636261cb6fcdc1e32a9
Jacob Martinez <jmartinez.data@gmail.com>
2020-01-29 09:03:27 -0600

    Initialization, Added Sum2DigitsInArray

---
+++ whiteboard/whiteboard/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whiteboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem #1
+            //int[] givenarray = { 5, 17, 77, 50 };
+            //int target = 55;
+            //Sum2DigitsInArray test1 = new Sum2DigitsInArray();
+            //test1.FindIndices(givenarray, target);
+
+            //Problem #2
+
+           
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ whiteboard/whiteboard/Sum2DigitsInArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whiteboard
+{
+    class Sum2DigitsInArray
+    {
+        /*
+        Given an array of integers, return indices of the two numbers such that 
+        they add up to a specific target.You may assume that each input would 
+        have exactly one solution, and you may not use the same element twice.
+            Use Case:
+                Given numbers in an array: [5, 17, 77, 50]
+                Target: 55
+        */
+
+        // Use a nested loop to iterate over list
+
+        public Sum2DigitsInArray()
+        {
+
+        }
+
+        public void FindIndices(int[] array, int target)
+        {
+            bool foundTarget = false;
+            if (foundTarget == false)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 1; j < array.Length; j++)
+                    {
+                        if (array[i] + array[j] == target)
+                        {
+                            Console.WriteLine("Found solution, {0} and {1} equal {2}", array[i], array[j], target);
+                            Console.WriteLine("The indices were {0} and {1}", i, j);
+                            foundTarget = true;
+                            i = j = array.Length; // To stop the nested loop, possibly use goto
+                            break;
+                        }
+                    }                    
+                }
+            }
+        }
+    }
+}
+
